4e4b12bc4bc0dbd548f84a0fe4dc6018de757e8b
Dmitry Novikov <dimka22081998@icloud.com>
2021-04-06 10:04:55 +0300

    Create project files
    Added:
    * MainMenuViewController.swift
    * Backend Boy Design.sketch
    
    Created:
    *Six .wav custom compositions for the game

---
+++ 's SSC Project/Backend Boy [Game Project].playground/Sources/Main Menu Screen/MainMenuViewController.swift
@@ -0,0 +1,17 @@
+import SpriteKit
+
+
+
+public final class MainMenuViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+        
+    }
+    
+    
+    
+    
+}
